3caca288e1bac343ee0ca3b91d550c15be7c8ce8
mahe-s45 <siliverumahesh.s@gmail.com>
2025-06-16 21:16:52 +0530

    VariablesDemo

---
+++ VariablesDemo.java
@@ -0,0 +1,13 @@
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+        //comments which are ignored by complier
+        //define variables
+        int age = 19;
+        String name = "Mahesh";
+        String message = "Welcome to Java Programming";
+        System.out.println(message);
+
+    }
+    
+}
