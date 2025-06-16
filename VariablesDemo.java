9f2d7f086160583cafe9b4ae3ec8e034cbb024b0
mahe-s45 <siliverumahesh.s@gmail.com>
2025-06-16 21:48:39 +0530

    define variables with data types

--- VariablesDemo.java
+++ VariablesDemo.java
@@ -2,11 +2,25 @@ public class VariablesDemo {
 
     public static void main(String[] args) {
         //comments which are ignored by complier
-        //define variables
-        int age = 19;
+        //define variables with data types
+        byte bytenumber = 100;
+        short shortNumber =1000;
+        int intNumber = 19;
+        float floatNumber = 3.7f;
+        double doublenumber = 3.145689;
+        boolean booleanValue = true; //false
+        char charValue = 'M';
+
+
         String name = "Mahesh";
         String message = "Welcome to Java Programming";
         System.out.println(message);
+        System.out.println(bytenumber);
+        System.out.println(intNumber);
+        System.out.println(floatNumber);
+        System.out.println(doublenumber);
+        System.out.println(booleanValue);
+        System.out.println(charValue);
 
     }
     
