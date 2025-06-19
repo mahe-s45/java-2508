afacff85f7a773b62734a3b1ebd75a27fefb4479
mahe-s45 <siliverumahesh.s@gmail.com>
2025-06-19 16:17:39 +0530

    Product Details Using DataTypes

--- DataTypes.java
+++ DataTypes.java
@@ -14,6 +14,37 @@ public class DataTypes {
         System.out.println("MRP: "+mrp);
         System.out.println("Discount: "+discountProduct+" %OFF");
         System.out.println("Selling Price: "+sellingPrice);
+
+
+        String company = "Mahindra";
+        String model = "Thar";
+        String modelDescription = "The Mahindra Thar is a lifestyle off-road SUV.";
+        double ratings = 4.5;
+        double minPriceinLakhs = 11.50;
+        double maxPriceinLakhs = 17.62;
+        int monthlyEMI = 31036;
+        int engineCCmin = 1497;
+        int engineCCmax = 2184;
+        int seatingCapacity = 4;
+        
+        System.out.println("Comapany: "+company);
+        System.out.println("Model: "+model);
+        System.out.println("Description: "+modelDescription);
+        System.out.println("Rating: "+ratings);
+        System.out.println("Price Range :"+ minPriceinLakhs +"-"+ maxPriceinLakhs + "LAKHS*");
+        System.out.println("EMI: "+monthlyEMI+ " per month");
+        System.out.println("Engine: "+engineCCmin +"CC - "+ engineCCmax +"CC");
+        System.out.println("Seating Capacity: "+seatingCapacity);
+        
+
+
+
+
+
+
+
+
+
         
 
 
