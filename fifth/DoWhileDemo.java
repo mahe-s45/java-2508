5e9b5e3699ea4d14f95dee018cb581d05b121be4
mahe-s45 <siliverumahesh.s@gmail.com>
2025-07-08 22:21:01 +0530

    Do while loop demo

---
+++ fifth/DoWhileDemo.java
@@ -0,0 +1,11 @@
+package fifth;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int pin = 7890;
+        do {
+            System.out.println("Welcome To SBI ATM");
+        } while (pin == 7890);
+    }
+
+}
