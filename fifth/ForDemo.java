4a17bcbb0af0cf7fe18385fcec4a716024ab2b7a
mahe-s45 <siliverumahesh.s@gmail.com>
2025-07-08 22:21:16 +0530

    For loop Demo

---
+++ fifth/ForDemo.java
@@ -0,0 +1,22 @@
+package fifth;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        
+        for (int i=0; i<=10; i++){
+            System.out.println("Hi");
+        }
+
+        // fill 5 water bottles
+        for (int i=1; i<=5; i++){
+            System.out.println("Filling Water Bottle: "+i);
+            
+            //fill only upto 2 bottles, we got only few people
+            if ( i==2) {
+                break;
+            }
+           
+        }
+    }
+
+}
