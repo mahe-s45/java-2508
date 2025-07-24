8909f2aeef7230786994798030550c946f6e9017
mahe-s45 <siliverumahesh.s@gmail.com>
2025-07-24 15:38:31 +0530

    Without this keywoed ambugity

--- eight/WithoutThis.java
+++ eight/WithoutThis.java
@@ -6,13 +6,13 @@ public class WithoutThis {
     int x;
     int y;
 
-    // explicit constructors without parameters -> what values you wanted, you defined
+    // explicit constructors without parameters --> what values you wanted, you defined
     public WithoutThis (){
         x = 100;
         y = 200;
     }
 
-    // explicit constructors with parameters -> what values you wanted, you defined
+    // explicit constructors with parameters --> what values you wanted, you defined
     public WithoutThis (int x, int y){
         // The above x & y are local Variables
        x = x;
